1539c2313ab8902f28a5830cbdd2c48120a159c5
Alex B <abailon949@gmail.com>
2020-04-02 10:09:47 -0700

    Grabbed all user data and put into an object

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-const questions = [
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+        type: 'input',
+        message: 'What is your user name?',
+        name: 'username'
+        }
+    ])
+    .then(({username}) => {
+        const queryUrl = `https://api.github.com/users/${ username }`;
+        axios
+        .get(queryUrl)
+        .then(function(res) {
+            let userData = {
+                userimg: res.data.avatar_url,
+                userName: res.data.login,
+                userLocation: res.data.location,
+                userGitHubURL: res.data.html_url,
+                userBlog: res.data.blog,
+                userBio: res.data.bio,
+                userPublicRepo: res.data.public_repos,
+                userFollowers: res.data.followers,
+                userFollowing: res.data.following
+            }
+
+            console.log(userData.userimg)
+        })
+    })
+    .catch(error => {
+        if(error) {
+          console.log(error)
+        }
+    })
+// const questions = [
   
-];
+// ];
 
-function writeToFile(fileName, data) {
+// function writeToFile(fileName, data) {
  
-}
+// }
 
-function init() {
+// function init() {
 
-init();
+    // }
+// init()
